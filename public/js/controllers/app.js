4ff60f6ab0ca68878436f7da2914e70326c1fc85
Milan Pal Singh <milanpalsingh89@gmail.com>
2014-12-26 20:41:39 +0530

    jason read edit

--- public/js/controllers/app.js
+++ public/js/controllers/app.js
@@ -5,30 +5,29 @@ var appCtrs = angular.module('appCtrs', []);
 appCtrs.controller('AppCtrl', ['$scope', '$http',
   	function($scope, $http) {
 
+  		// logic for the main navigation 
   		$scope.setActive = function(page){
   			$scope.homeActive='';
 		    $scope.resumeActive='';
 		    $scope.errorActive='';
-
 		    $scope[page+'Active'] = 'active';
   		};
 
 	    $http.get('res/config.json').success(function(data) {
 	    	$scope.config = data;
-	    	console.log($scope.config);
-	    	return data;
 	    });
 }]);
 
 // Defining controller for URL '/resume'. Called on portfolioApp.js routing.
 appCtrs.controller('HomeCtrl', ['$scope', '$http',
   	function($scope, $http) {
+  		// main navigation - set home tab active
   		$scope.setActive('home');
-	    console.log("inside the home controller");
 }]);
 
 
 appCtrs.controller('ErrorCtrl', ['$scope', '$http',
   	function($scope, $http) {
+  		// main navigation - set error tab active
   		$scope.setActive('error');
 }]);
--- public/js/controllers/resume.js
+++ public/js/controllers/resume.js
@@ -4,11 +4,15 @@ var resumeCtrs = angular.module('resumeCtrs', []);
 // Defining controller for URL '/resume'. Called on portfolioApp.js routing.
 resumeCtrs.controller('ResumeBuildCtrl', ['$scope', '$http',
   	function($scope, $http) {
+  		// main navigation - set resume tab active
   		$scope.setActive('resume');
 
-	    console.log($scope.config);
-	    // $http.get('res/config.json').success(function(data) {
-	    // 	$scope.config = data;
-	    // 	console.log($scope.config);
-	    // });
+	    console.log($scope.config.resume);
+	    $http.get($scope.config.resume).success(function(data) {
+	    	for (var key in data) {
+			   console.log(' name=' + key + ' value=' + data[key]);
+
+			}
+	    	console.log(data);
+	    });
 	}]);
