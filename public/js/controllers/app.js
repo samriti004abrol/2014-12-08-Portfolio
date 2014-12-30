2e6370178708194a9142645b7276c43ad605cc99
Milan Pal Singh <milanpalsingh89@gmail.com>
2014-12-30 21:33:50 +0530

    resume built

--- public/js/controllers/app.js
+++ public/js/controllers/app.js
@@ -3,26 +3,26 @@ var appCtrs = angular.module('appCtrs', []);
 
 // main controller. 
 appCtrs.controller('AppCtrl', ['$scope', '$http',
-  	function($scope, $http) {
+  function($scope, $http) {
 
-  		// logic for the main navigation 
-  		$scope.setActive = function(page){
-  			$scope.homeActive='';
-		    $scope.resumeActive='';
-		    $scope.errorActive='';
-		    $scope[page+'Active'] = 'active';
-  		};
+  // logic for the main navigation 
+  $scope.setActive = function(page){
+  	$scope.homeActive='';
+    $scope.resumeActive='';
+    $scope.errorActive='';
+    $scope[page+'Active'] = 'active';
+  };
 
-	    $http.get('res/config.json').success(function(data) {
-	    	$scope.config = data;
-	    });
+  $http.get('res/config.json').success(function(data) {
+  	$scope.config = data;
+  });
 }]);
 
 // Defining controller for URL '/resume'. Called on portfolioApp.js routing.
 appCtrs.controller('HomeCtrl', ['$scope', '$http',
-  	function($scope, $http) {
-  		// main navigation - set home tab active
-  		$scope.setActive('home');
+  function($scope, $http) {
+  	// main navigation - set home tab active
+  	$scope.setActive('home');
 }]);
 
 
--- public/js/controllers/resume.js
+++ public/js/controllers/resume.js
@@ -1,18 +1,45 @@
 // Defining Anguler module. Called on portfolioApp.js 
 var resumeCtrs = angular.module('resumeCtrs', []);
 
+var resumeHeadings = [];
+
 // Defining controller for URL '/resume'. Called on portfolioApp.js routing.
 resumeCtrs.controller('ResumeBuildCtrl', ['$scope', '$http',
   	function($scope, $http) {
   		// main navigation - set resume tab active
   		$scope.setActive('resume');
 
+  		$scope.resume= {};
+
 	    console.log($scope.config.resume);
+	    
+	    // read resume config file
 	    $http.get($scope.config.resume).success(function(data) {
 	    	for (var key in data) {
-			   console.log(' name=' + key + ' value=' + data[key]);
-
+			   resumeHeadings.push(key);
+			   $scope[key]={};
+			   $scope[key].val = "value";
 			}
-	    	console.log(data);
+
+			   console.log($scope.Skill.val);
+
+			// demo($scope, $http);
+		  //   $http.get('res/resume/skills.json').success(function(data) {
+		  //   	for (var key in data) {
+			 //   		resumeHeadings.push(key);
+				// }
+		  //   });
+
+			$scope.resumeHeadings = resumeHeadings;
+			$scope.resumeConfig = data;
+	    	console.log(resumeHeadings);
 	    });
-	}]);
+}]);
+
+var demo = function($scope, $http){
+	$http.get('res/resume/skills.json').success(function(data) {
+		for (var key in data) {
+	   		resumeHeadings.push(key);
+		}
+    });	
+};
